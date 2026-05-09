9c90f5ee1fe3b0e32f0c8dfb0c92b28daa720e38
fazxog2rt-svg <fazxog2rt@gamil.com>
2026-05-09 10:04:15 +0700

    Fix Vercel Prisma Config

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,6 +3,6 @@ import 'dotenv/config';
 
 export default defineConfig({
   datasource: {
-    url: process.env.DIRECT_URL,
+    url: process.env.DIRECT_URL || process.env.DATABASE_URL || 'postgresql://dummy',
   },
 });
